aa326ca97fbdafa1efee44f99f78cc585927d186
GH0STAV0 <you@example.com>
2023-01-05 10:01:48 +0100

    docker with node

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Version : V-09 [ 29-12-20 ] YH84 \
+  res.json({ message: "Version : V-10 [ 05 01 2023 ] YH84 \
             PUT /google_account/activate/:customerId5 ; /google_account/update/:customerId4 ; GET  /van/1 ;   GET /van/ran ; GET  /van/count   ; PUT /van/reset_all ; PUT /van/update/1  ; GET /van/ran ; GET  /van/count   ; GET /google_account/active ; GET  /nor/1 ;   GET /nor/ran ; GET  /nor/count   ; PUT /nor/reset_all ; PUT /nor/update/1  " });
 });
 
 require("./app/routes/customer.routes.js")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
